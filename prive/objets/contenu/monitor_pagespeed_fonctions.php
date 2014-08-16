7c6aee318dd219b8e711fd55c8ead071f4f4710d
magikcypress <cyp@rouquin.me>
2014-08-16 16:35:37 +0200

    upgrade pagespeed fonction

---
+++ prive/objets/contenu/monitor_pagespeed_fonctions.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Pipeline pour Monitor
+ *
+ * @plugin     Monitor
+ * @copyright  2014
+ * @author     Vincent
+ * @licence    GNU/GPL
+ * @package    SPIP\Monitor\fonctions
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function monitor_pageSpeed($href) {
+
+    if (lire_config('monitor/activer_monitor') == "oui") {
+        
+        include_once(_DIR_PLUGIN_MONITOR."lib/Monitor/MonitorSites.php");
+
+        $result[] = getPageSpeedGoogle($href);
+    }
+
+    return $result;
+}
+
+function monitor_tableau_pagestats($texte) {
+
+    if(isset($texte)) {
+        foreach ($texte as $cle => $valeur) {
+            $pagestats[$cle] = $valeur;
+        }
+    }
+    
+    return $pagestats;
+}
