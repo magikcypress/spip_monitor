e72a8a1f2f7e12c40ea08231edf93eb150da8f15
magikcypress <cyp@rouquin.me>
2014-11-04 11:32:22 +0100

    ajout du cron stats techniques

--- base/monitor.php
+++ base/monitor.php
@@ -21,6 +21,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 function monitor_declarer_tables_interfaces($interfaces) {
 	$interfaces['table_des_tables']['monitor'] = 'monitor';
 	$interfaces['table_des_tables']['monitor_log'] = 'monitor_log';
+	$interfaces['table_des_tables']['monitor_stats'] = 'monitor_stats';
+	$interfaces['table_des_tables']['monitor_stats_plugins'] = 'monitor_stats_plugins';
 	
 	return $interfaces;
 }
@@ -29,6 +31,7 @@ function monitor_declarer_tables_objets_sql($tables){
 	// On ajoute un champ date_ping et statut_log dans spip_syndic
 	$tables['spip_syndic']['field']['date_ping'] = "datetime NOT NULL";
 	$tables['spip_syndic']['field']['statut_log'] = "varchar(3) NOT NULL";
+	$tables['spip_syndic']['field']['statut_stats'] = "varchar(3) NOT NULL";
 
 	$tables['spip_monitor'] = array(
 		'texte_retour' => 'icone_retour',
@@ -44,7 +47,7 @@ function monitor_declarer_tables_objets_sql($tables){
 			"type"	=> "varchar(255) NOT NULL",
 			"statut" => "varchar(255) NOT NULL default 'oui'",
 			"date_modif" => "datetime DEFAULT '0000-00-00 00:00:00' NOT NULL",
-			"maj"	=> "TIMESTAMP"
+			"maj"	=> "TIMESTAMP",
 		),
 		'key' => array(
 			"PRIMARY KEY"	=> "id_monitor",
@@ -66,6 +69,40 @@ function monitor_declarer_tables_objets_sql($tables){
 			"PRIMARY KEY"	=> "id_monitor_log",
 		)
 	);
+
+	$tables['spip_monitor_stats'] = array(
+		'principale' => 'non',
+		'field'=> array(
+			"id_monitor_stats" => "bigint(21) unsigned NOT NULL AUTO_INCREMENT",
+			"id_syndic" => "bigint(21) NOT NULL",
+			"ip" => "varchar(255) default '' NOT NULL",
+			"spip" => "varchar(255) default '' NOT NULL",
+			"server" => "varchar(255) default '' NOT NULL",
+			"php" => "varchar(255) default '' NOT NULL",
+			"gzip" => "varchar(3) default '' NOT NULL",
+			"plugins" => "bigint(21) default NULL",
+			"pays" => "char(3) default '' NOT NULL",
+			"date" => "datetime DEFAULT '0000-00-00 00:00:00' NOT NULL",
+			"retry" 	=> "int(5) default 0 NOT NULL",
+			"status" 	=> "varchar(10) default '' NOT NULL",
+		),
+		'key' => array(
+			"PRIMARY KEY"	=> "id_monitor_stats",
+		)
+	);
+
+	$tables['spip_monitor_stats_plugins'] = array(
+		'principale' => 'non',
+		'field'=> array(
+			"id_monitor_stats"    => "bigint(21) NOT NULL",
+			"plugin" => "varchar(64) default '' NOT NULL",
+			"version" => "varchar(255) default '' NOT NULL",
+		),
+		'key' => array(
+			"PRIMARY KEY"	=> "id_monitor_stats,plugin",
+		)
+	);
+
 
 	return $tables;
 }
--- monitor_administrations.php
+++ monitor_administrations.php
@@ -23,7 +23,7 @@ function monitor_upgrade($nom_meta_base_version,$version_cible){
 	$maj = array();
 
 	$maj['create'] = array(
-		array('maj_tables', array('spip_monitor', 'spip_monitor_log', 'spip_syndic'))
+		array('maj_tables', array('spip_monitor', 'spip_monitor_log', 'spip_syndic', 'spip_monitor_stats', 'spip_monitor_stats_plugins'))
 	);
 
 	$maj['1.1'] = array(	
@@ -32,7 +32,13 @@ function monitor_upgrade($nom_meta_base_version,$version_cible){
 	);
 
 	$maj['1.2'] = array(
+		// Ajout du champs alert
 		array('sql_alter',"TABLE spip_monitor ADD alert int(11) DEFAULT 0 NOT NULL AFTER id_syndic"));
+
+	$maj['1.3'] = array(	
+		// Ajout de la table spip_monitor_stats, spip_monitor_stats_plugins
+	 	array('maj_tables', array('spip_syndic', 'spip_monitor_stats', 'spip_monitor_stats_plugins'))
+	);
 
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
@@ -47,7 +53,11 @@ function monitor_vider_tables($nom_meta_base_version) {
 
 	sql_drop_table("spip_monitor");
 	sql_drop_table("spip_monitor_log");
+	sql_drop_table("spip_monitor_stats");
+	sql_drop_table("spip_monitor_stats_plugins");
 	sql_alter('TABLE spip_syndic DROP COLUMN date_ping');
+	sql_alter('TABLE spip_syndic DROP COLUMN statut_log');
+	sql_alter('TABLE spip_syndic DROP COLUMN statut_stats');
 	effacer_meta("monitor");
 	effacer_meta($nom_meta_base_version);
 }
