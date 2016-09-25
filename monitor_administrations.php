af69bb9be162e20aaf459789349c914ae9a0e6ac
cyp <cyp@rouquin.me>
2016-09-25 13:27:34 +0200

    Oups: Modification champs sur la table spip_monitor_evenements

--- monitor_administrations.php
+++ monitor_administrations.php
@@ -78,7 +78,8 @@ function monitor_upgrade($nom_meta_base_version, $version_cible) {
 		// Suppression de unsigned et auto incremente pour compat sqlite
 		array('sql_alter', 'TABLE spip_monitor MODIFY id_monitor BIGINT(21) NOT NULL AUTO_INCREMENT'),
 		array('sql_alter', 'TABLE spip_monitor_log MODIFY id_monitor_log BIGINT(21) NOT NULL AUTO_INCREMENT'),
-		array('sql_alter', 'TABLE spip_monitor_stats MODIFY id_monitor_stats BIGINT(21) NOT NULL AUTO_INCREMENT')
+		array('sql_alter', 'TABLE spip_monitor_stats MODIFY id_monitor_stats BIGINT(21) NOT NULL AUTO_INCREMENT'),
+		array('sql_alter', 'TABLE spip_monitor_evenements MODIFY id_monitor_evenements BIGINT(21) NOT NULL AUTO_INCREMENT')
 	);
 
 	include_spip('base/upgrade');
